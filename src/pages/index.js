1b8b1433047047181bc5091f226093a7a0f7dbdb
ManishKuPatra254 <manishpatra254@gmail.com>
2024-05-22 19:49:38 +0530

    started new project on gatsby framework

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
+// import * as styles from "../components/index.module.css"
 
 const IndexPage = () => (
   <Layout>
-    <div className={styles.textCenter}>
+    <div className="">
 
     </div>
   </Layout>
